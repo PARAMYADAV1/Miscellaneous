b02c015f5945e13536e9146c9b297c6ff15e4632
PARAMYADAV1 <pramhanspramhans421@gmail.com>
2026-09-08 22:12:49 +0530

    initial project2

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/register",(req,res)=>{
+    let {user , pass } = req.query;
+   
+    res.send(`standard Get response. Welcome ${user}!`);
+    app.use(express.json);
+});
+app.post("/register",(req,res)=>{
+   
+    console.log(req.body);
+    let {user,pass}=req.body;
+    res.send(`standard Post response ${user}`);
+});
+app.listen(port,()=>{
+    console.log(`listening to port ${port}`);
+});
