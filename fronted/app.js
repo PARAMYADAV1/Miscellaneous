493cbc438065d75b9619bd382adcb7b63aa77960
PARAMYADAV1 <pramhanspramhans421@gmail.com>
2026-09-10 07:42:55 +0530

    update

--- fronted/app.js
+++ fronted/app.js
@@ -1,2 +1,8 @@
-let arr = [1,2,3,4];
-console.log(arr);
+let arr1 = [1,2,3,4];
+let arr2= [1,2,3,4];
+arr1.sayhello = ()=>{
+     console.log("hello to param");
+}
+arr2.sayhello = ()=>{
+     console.log("hello to param");
+}
